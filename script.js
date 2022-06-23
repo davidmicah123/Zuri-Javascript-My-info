fb14143eb1a5042b546b9a4634bcbd996a049548
David Micah-Daniels <98872908+davidmicah123@users.noreply.github.com>
2022-06-23 07:45:02 +0100

    Saving changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let my_name = "David micah-Daniels";
+let my_name = "David Micah-Daniels";
 let height = 4;
 let country = "Nigeria"
-alert(`My name is ${my_name}, i am ${height}ft tall and i am a citizen of ${country}`);
+alert(`My name is ${my_name}, i am ${height}feet tall and i am a citizen of ${country}`);
